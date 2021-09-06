9790c4e971825681687c7086451b7bde1023388e
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2021-09-06 10:30:07 +0200

    WIP: added validation for provider additional codes

---
+++ Model/Command/ProviderAdditionalConfiguration/ValidateConfigurationCodes.php
@@ -0,0 +1,79 @@
+<?php
+namespace MageSuite\ErpConnector\Model\Command\ProviderAdditionalConfiguration;
+
+class ValidateConfigurationCodes
+{
+    const ERROR_MESSAGE = 'Additional configuration for "%s" key is set in "%s" provider. Please remove it from provider first.';
+
+    /**
+     * @var \MageSuite\ErpConnector\Helper\Configuration
+     */
+    protected $configuration;
+
+    /**
+     * @var \MageSuite\ErpConnector\Model\ResourceModel\ProviderAdditionalConfiguration
+     */
+    protected $resourceModel;
+
+    public function __construct(
+        \MageSuite\ErpConnector\Helper\Configuration $configuration,
+        \MageSuite\ErpConnector\Model\ResourceModel\ProviderAdditionalConfiguration $resourceModel
+    ) {
+        $this->configuration = $configuration;
+        $this->resourceModel = $resourceModel;
+    }
+
+    public function execute($configurationCodes)
+    {
+        $changedCodes = $this->getChangedCodes($configurationCodes);
+
+        if (empty($changedCodes)) {
+            return true;
+        }
+
+        $providerNames = $this->resourceModel->getProviderNamesWithSpecificAdditionalConfig($changedCodes);
+
+        if (empty($providerNames)) {
+            return [];
+        }
+
+        $errorMessages = [];
+
+        foreach ($providerNames as $providerName) {
+            $errorMessages[] = sprintf(self::ERROR_MESSAGE, $providerName['key'], $providerName['name']);
+        }
+
+        return $errorMessages;
+    }
+
+    protected function getChangedCodes($newConfigurationCodes)
+    {
+        $newCodes = [];
+
+        foreach ($newConfigurationCodes as $newCode) {
+            if (empty($newCode)) {
+                continue;
+            }
+
+            $newCodes[$newCode['label']] = $newCode['value'];
+        }
+
+        $changedCodes = [];
+
+        foreach ($this->configuration->getProviderAdditionalConfigurationCodes() as $currentCode) {
+            if (empty($currentCode)) {
+                continue;
+            }
+
+            if (!isset($newCodes[$currentCode['label']])) {
+                //code was removed
+                $changedCodes[] = $currentCode['value'];
+            } elseif ($newCodes[$currentCode['label']] != $currentCode['value']) {
+                //value was changed
+                $changedCodes[] = $currentCode['value'];
+            }
+        }
+
+        return $changedCodes;
+    }
+}
--- Model/ResourceModel/ProviderAdditionalConfiguration.php
+++ Model/ResourceModel/ProviderAdditionalConfiguration.php
@@ -7,4 +7,21 @@ class ProviderAdditionalConfiguration extends \Magento\Framework\Model\ResourceM
     {
         $this->_init('erp_connector_provider_additional_configuration', \MageSuite\ErpConnector\Api\Data\ProviderAdditionalConfigurationInterface::ID);
     }
+
+    public function getProviderNamesWithSpecificAdditionalConfig($codes)
+    {
+        $connection = $this->getConnection();
+
+        $query = $connection
+            ->select()
+            ->from(['pac' => $connection->getTableName('erp_connector_provider_additional_configuration')], ['entity_id', 'key'])
+            ->joinLeft(['p' => $connection->getTableName('erp_connector_provider')], 'pac.provider_id = p.provider_id', 'name')
+            ->where('pac.key in (?)', $codes);
+
+        try {
+            return $connection->fetchAll($query);
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
 }
