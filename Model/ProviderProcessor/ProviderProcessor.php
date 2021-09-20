d6062e58360a33b8d5d0ee9e33771c4cbe9aadc6
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2021-09-20 19:23:46 +0200

    Adjusted scheduler logic

--- Model/ProviderProcessor/ProviderProcessor.php
+++ Model/ProviderProcessor/ProviderProcessor.php
@@ -4,15 +4,73 @@ namespace MageSuite\ErpConnector\Model\ProviderProcessor;
 
 class ProviderProcessor extends \Magento\Framework\DataObject
 {
-    protected $scheduler;
+    /**
+     * @var \MageSuite\ErpConnector\Api\ProviderRepositoryInterface
+     */
+    protected $providerRepository;
+
+    /**
+     * @var \MageSuite\ErpConnector\Model\Command\LogErrorMessage
+     */
+    protected $logErrorMessage;
+
+    protected $scheduler = null;
+
+    protected $provider = null;
+
+    public function __construct(
+        \MageSuite\ErpConnector\Api\ProviderRepositoryInterface $providerRepository,
+        \MageSuite\ErpConnector\Model\Command\LogErrorMessage $logErrorMessage,
+        array $data = []
+    ) {
+        parent::__construct($data);
+
+        $this->providerRepository = $providerRepository;
+        $this->logErrorMessage = $logErrorMessage;
+    }
+
+    public function execute()
+    {
+        return true;
+    }
 
     public function setScheduler($scheduler)
     {
         $this->scheduler = $scheduler;
     }
 
-    public function execute()
+    protected function getProvider()
     {
-        return true;
+        if ($this->scheduler === null) {
+            throw new \Exception('Scheduler isn\'t set.'); //phpcs:ignore
+        }
+
+        if ($this->provider !== null) {
+            return $this->provider;
+        }
+
+        $this->provider = $this->providerRepository->getById($this->scheduler->getProviderId());
+        return $this->provider;
+    }
+
+    protected function processErrorMessage($e)
+    {
+        $title = __('%1 provider orders export ERROR.', $this->getProvider()->getName());
+        $messages = [
+            $title,
+            __($e->getMessage())
+        ];
+
+        $previous = $e->getPrevious();
+
+        while ($previous) {
+            $msg[] = __($previous->getMessage());
+            $previous = $previous->getPrevious();
+        }
+
+        $messages = implode(' ', $messages);
+        $this->logErrorMessage->execute($this, $messages, null, \Magento\Framework\Notification\MessageInterface::SEVERITY_MINOR);
+
+        throw new \Creativestyle\CustomizationIpetOrder\Exception\OrderExport($messages, 0, $e);
     }
 }
