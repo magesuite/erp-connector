2e2fa6b2a91e1e237cd24fcc674f5c52eb881848
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2021-11-12 06:17:51 +0100

    Remove objects from logging

--- Model/Command/LogErrorMessage.php
+++ Model/Command/LogErrorMessage.php
@@ -26,10 +26,28 @@ class LogErrorMessage
     public function execute($title, $message, $data = null, $severity = \Magento\Framework\Notification\MessageInterface::SEVERITY_CRITICAL) //phpcs:ignore
     {
         if ($data !== null) {
+            $data = $this->prepareData($data);
             $message = sprintf(self::MESSAGE_WITH_DATA_FORMAT, $message, var_export($data, true));
         }
 
         $this->logger->error($message);
         $this->addAdminNotification->execute($title, $message, $severity);
     }
+
+    protected function prepareData($data)
+    {
+        $result = [];
+
+        foreach ($data as $key => $value) {
+            if (is_object($value)) {
+                $result[$key] = null;
+                continue;
+            }
+
+            $result[$key] = $value;
+        }
+
+        return $result;
+    }
+
 }
