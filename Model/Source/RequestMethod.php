a9fa2894e6bfe7a564bf42cd260efd916b4d741e
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2021-11-17 09:30:35 +0100

    make the labels in requestmethod uppercased

--- Model/Source/RequestMethod.php
+++ Model/Source/RequestMethod.php
@@ -6,10 +6,10 @@ class RequestMethod implements \Magento\Framework\Data\OptionSourceInterface
     public function toOptionArray()
     {
         return [
-            ['value' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_GET, 'label' => __('Get')],
-            ['value' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_POST, 'label' => __('Post')],
-            ['value' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_PUT, 'label' => __('Put')],
-            ['value' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_DELETE, 'label' => __('Delete')],
+            ['value' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_GET, 'label' => __('GET')],
+            ['value' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_POST, 'label' => __('POST')],
+            ['value' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_PUT, 'label' => __('PUT')],
+            ['value' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_DELETE, 'label' => __('DELETE')],
         ];
     }
 }
